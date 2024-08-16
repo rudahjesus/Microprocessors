6978a8aa4ed3f988924121086bb64c06f98c4be3
Rudah S Jesus <156484678+rudahjesus@users.noreply.github.com>
2024-08-16 17:48:39 -0300

    Update main.c

--- c_chama_ass_maciel/main.c
+++ c_chama_ass_maciel/main.c
@@ -1,4 +1,5 @@
 //PROGRAM BY MORCGOPOCILGAXD
+
 #include <reg52.h>
 
 extern void led_on(char n);
@@ -6,16 +7,18 @@ extern void led_off(char n);
 extern void delay(void);
 
 void main(void){
-  char i;
+  char i,j;
   while (1){
-    for (i = 7; i >= 0; i--){
-      led_on(i);
+    for (j = 7; j>= 0; j--){
+      led_on(j);
+      if( j < 0){
+         for (i = 0; i > 8; i++){
+           led_off(i);
+    }   
+       }
       delay();
-       // led_off(i); // Opcional: Apaga o LED atual antes de acender o próximo.
     }
-		for(i = 0; i < 8; i++){
-		  led_off(i);
-			delay();
-		}
+
+
   }
 }
