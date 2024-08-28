39f6e72a954d47e3f72b06b99bd526886dd89b64
Rudah S Jesus <156484678+rudahjesus@users.noreply.github.com>
2024-08-27 23:13:42 -0300

    Update flash-control-explicado.asm

--- flash_control/flash-control-explicado.asm
+++ flash_control/flash-control-explicado.asm
@@ -5,7 +5,7 @@ LAMP1     equ       P1.0
 LAMP2     equ       P1.1
 LAMP3     equ       P1.2
 LAMP4     equ       P1.3
-
+;************************************************************************************************
          cseg    at 0
 
 inicio:  mov    TMOD,#20h          ; Timer 1 em modo 2 (auto-reload)
@@ -21,6 +21,8 @@ volta:   jnb    RI,$               ; Espera receber um caractere
          clr    RI                 ; Limpa flag de recepção
          mov    a,SBUF             ; Move o caractere recebido para A
 
+;************************************************************************************************
+;VERIFICA
 
          cjne   a,#'1',verifica_2
          cpl    LAMP1              ; Inverte o estado da lâmpada 1
@@ -58,45 +60,96 @@ verifica_6:  cjne   a,#'6',verifica_7
          
 
 verifica_7:  cjne   a,#'7',volta
-             call   verificar_lampadas ; Verifica e envia o status das lâmpadas
+             call   verificar_L1 ; Verifica e envia o status das lâmpadas
              jmp    volta              ; Volta para esperar o próximo comando
-         
 
-verificar_lampadas:  mov    dptr,#L1a
-                     jnb     LAMP1,enviar_L1    ; Se LAMP1 está ligada, envia mensagem
-                     jmp    verificar_L2       ; Caso contrário, vai para a próxima lâmpada
+;*********************************************************************************************************************************************************         
+;O CÓDIGO ESTÁ DIVIDIDO ENTRE LNX, LX E LTX. LX É UMA OCASIÃO EM QUE A LAMPADA 1 NÃO ESTÁ LIGADA, LOGO, A SITUAÇÃO DE TODAS LIGADAS NÃO PODE OCORRER
+;LTX É UMA OCASIÃO EM QUE A LÂMPADA 1 ESTÁ LIGADA, PULA PARA VERIFICAR A PRÓXIMA EM UMA SEQUÊNCIA EM CADEIA, E NO FINAL, PRINTA QUE TODAS ESTÃO ACESAS
+;LNX É TODAS DESLIGADAS
+
+;************************************************************************************************
+;LN1 / LT1
+
+verificar_L1:  mov    dptr,#L1a
+               jnb    LAMP1,enviar_LT1    ; Se LAMP1 está ligada, pula para ENVIAR LT1 e envia mensagem
+               jmp    verificar_LN2       ; Caso contrário, pula para VERIFICAR L2 vai para a próxima lâmpada
                      
 
-enviar_L1:   call   print
-             jmp    verificar_L2
-             
-
-verificar_L2:  mov    dptr,#L2a
-               jnb     LAMP2,enviar_L2    ; Se LAMP2 está ligada, envia mensagem
+enviar_LT1:   call   print               ; PRINTA MENSAGEM
+              jmp    verificar_LT2       ; PULA PARA VERIFICAR LT2
+			 
+;************************************************************************************************
+;LN2 / L2 / LT2
+verificar_LN2:  mov    dptr,#L2a
+               jnb    LAMP2,enviar_L2    ; Se LAMP2 está ligada, envia mensagem
+               jmp    verificar_LN3       ; Caso contrário, vai para a próxima lâmpada
+			   
+verificar_LT2: mov    dptr,#L2a
+			   jnb    LAMP2,enviar_LT2    ; Se LAMP2 está ligada, envia mensagem
                jmp    verificar_L3       ; Caso contrário, vai para a próxima lâmpada
                
-
 enviar_L2:  call   print
-            jmp    verificar_L3
-         
+            jmp    verificar_L3          
+			
+enviar_LT2: call   print
+            jmp    verificar_LT3
+			
+;************************************************************************************************
+;LN3 / L3 / LT3
 
 verificar_L3:  mov    dptr,#L3a
-               jnb     LAMP3,enviar_L3    ; Se LAMP3 está ligada, envia mensagem
+               jnb    LAMP3,enviar_L3    ; Se LAMP3 está ligada, envia mensagem
                jmp    verificar_L4       ; Caso contrário, vai para a próxima lâmpada
-         
+
+verificar_LT3: mov    dptr,#L3a
+               jnb    LAMP3,enviar_LT3    ; Se LAMP2 está ligada, envia mensagem
+               jmp    verificar_L4       ; Caso contrário, vai para a próxima lâmpada
+			   
+verificar_LN3: mov    dptr,#L3a
+               jnb    LAMP3,enviar_L3    ; Se LAMP2 está ligada, envia mensagem
+               jmp    verificar_LN4       ; Caso contrário, vai para a próxima lâmpada
 
 enviar_L3: call   print
            jmp    verificar_L4
-         
+		   
+enviar_LT3: call   print
+            jmp    verificar_LT4
+
+;************************************************************************************************		   
+;LN4 / L4 / LT4        
 
 verificar_L4:  mov    dptr,#L4a
-               jnb     LAMP4,enviar_L4    ; Se LAMP4 está ligada, envia mensagem
-               ret                       ; Retorna após verificar todas as lâmpadas
-
-         
+               jnb    LAMP4,enviar_L4    ; Se LAMP4 está ligada, envia mensagem
+			   ret
+			   
+verificar_LT4:  mov    dptr,#L4a
+                jnb    LAMP4, enviar_LT4
+                ret
+				
+verificar_LN4: mov    dptr,#L4a
+               jnb    LAMP4,enviar_L4    ; Se LAMP2 está ligada, envia mensagem
+               jmp    verificar_Ln       ; Caso contrário, vai para a próxima lâmpad
+				
 enviar_L4:  call   print
             ret
-         
+			
+enviar_LT4: call   print
+            jmp verificar_LTT
+
+;************************************************************************************************			
+;TODOS / NENHUM
+				
+verificar_LTT:  mov    dptr,#Ltt
+                call   print
+                ret
+				
+verificar_Ln:  mov   dptr,#Ln
+               call  print
+	      ret
+
+;************************************************************************************************
+;PRINTA 
 
 print:  clr    a
         movc   a,@a+dptr
@@ -124,4 +177,8 @@ L1a:     db     "Lampada 1 acesa",0dh,0ah,0
 L2a:     db     "Lampada 2 acesa",0dh,0ah,0
 L3a:     db     "Lampada 3 acesa",0dh,0ah,0
 L4a:     db     "Lampada 4 acesa",0dh,0ah,0
+	
+Ltt:     db     "Todas acesas",0dh,0ah,0
+Ln:      db     "Todas apagados",0dh,0ah,0
+
          end
