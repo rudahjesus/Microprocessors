0a447e837b78a78cd7aacad07d021b3094e45f5c
wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-09-20 16:00:33 -0300

    Update questao5.asm

--- ferias_asm/questao5.asm
+++ ferias_asm/questao5.asm
@@ -1,30 +1,33 @@
-; code by M0RC3G0P0C1LG4
+; ; code by M0RC3G0P0C1LG4
 
-LED1      equ       p1.0
-LED2      equ       p1.1
-LED3      equ       p1.2
-entrada   equ       p1.3
+; LED1      equ       p1.0
+; LED2      equ       p1.1
+; LED3      equ       p1.2
+; entrada   equ       p1.3
 
-cseg
+; 				cseg at 0
 
-inicio:       setb     LED1
-              setb     LED2
-              setb     LED3
+; inicio:       setb     LED1
+;               setb     LED2
+;               setb     LED3
 
-verificasubtensão: cjne entrada,#90h, subtensão
-									 jnc entrada, #0h, subtensão
+verificasubtensão: cjne entrada,#90h, dif90
+				   jmp subtensão
 
-									 jmp verificanormal
+dif90:	jc entrada, subtensão
+		cjne  entrada, #127, dif127
+		jmp normal
 
-verificanormal: jnc entrada, #90h, normal
-								jc entrada, #127, normal
+dif127: jc entrada, normal
+		jnc entrada, sobretensão
 
-							  jmp verificasobretensão
 
-verificasobretensão: jnc entrada,#127, sobretensão
+; verificanormal: jnc entrada, #90h, normal
+; 				jc entrada, #127, normal
+; 				jmp verificasobretensão
+
+; verificasobretensão: jnc entrada,#127, sobretensão
 
 subtensão: clr LED1
-
 normal: clr LED2
-
 sobretensão: clr LED3
