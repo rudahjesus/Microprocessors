ff7ef2c9b29a043a2f682516847ec79d8836cafa
wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-10-16 12:20:01 -0300

    Add files via upload

---
+++ escalonador_task.c
@@ -0,0 +1,42 @@
+/*MADE BY TURMINHA DO DIDI XD*/
+
+void task1(void){
+  print(x,y imagem);
+    switch(estado){
+	  case 1:
+	    x++;
+	    y--;
+	    if(x>=(240-21)){
+	      estado = 0;
+	    }
+	    if(y <=0){
+	      if(estado == 0){
+		    estado = 3;
+		  } 
+		  else{
+		    estado = 2;
+		  }
+	    }
+	    break;
+	    case 0:
+	      x--;
+	      y--;
+		  if(x<=(240-21)){
+		    estado = 
+		  }
+	  
+	    break;
+	    case 3:
+		x--;
+		y++;
+		case 2:
+		x++;
+		y--;
+      }
+}
+
+void task2 (void){
+}
+
+void task3(void){ 
+}
