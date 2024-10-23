77a54c3a199c2e7a6ce9a1e7dcae081f9abd07a3
wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-10-23 12:15:15 -0300

    Update main.c

--- escalonador/main.c
+++ escalonador/main.c
@@ -1,11 +1,13 @@
  /*MADE BY TURMINHA DO DIDI XD*/
  /*bancada 3  1032*/
+#include <reg52.h>
 
+extern char estado;
  void main(void){
    TMOD = 0x01;
    TH0=(256-78);
    IE = 0x82;
-   TR0 - 1;
+   TR0 = 1;
+   estado = 0;
    while(1);
- 
  }
