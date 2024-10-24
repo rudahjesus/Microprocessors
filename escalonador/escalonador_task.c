2a2178cfa2fd0fb7ac504390ba988e003314a437
wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-10-23 22:58:53 -0300

    Update escalonador_task.c

--- escalonador/escalonador_task.c
+++ escalonador/escalonador_task.c
@@ -1,50 +1,222 @@
 /*MADE BY TURMINHA DO DIDI XD*/
 #include <reg52.h>
-#include "didicolher_bmp.c"
-#include "Z:\Rudah_Mariana\lib\sll.h"
+#include "DIDICOLHER_bmp.c"
+#include "C:\Keil_v5\Microprocessors-main\escalonador\sll.h"
 int x;
 int y;
 extern char estado;
 
 void task1(void){
-  printbmp(0,0, didicolher);
-  paintscr(255,255,255);
-  while(1);
+  printbmp(x,y, DIDICOLHER);
+  paintscr(0,160,192);
 
     switch(estado){
-	  case 1:
-	    x++;
-	    y--;
-	    if(x>=(240-21)){
-	      estado = 0;
+		case 1:  
+		    x--;
+		    y--;
+
+			if(x<=0){
+				estado = 3;	
+			}
+			if(y<=0){
+              if(estado == 3){
+				estado = 2;
+			  }
+			}
+			else{
+				estado = 0;
+			}
+	
+
+	    case 0:  
+		    x--;
+		    y++;
+			if(x<=0){
+				estado = 1;
+			}
+			if(y>=(240-21)){
+				if(estado == 1)
+				estado = 3;
+			}
+			else{
+				estado = 2;
+			}
+		break;	    
+    
+        case 3:  
+	        x++;
+	        y--;
+	        if(x>=(240-21)){
+	          estado = 0;
+	        }
+	        if(y <=0){
+	          if(estado == 0){
+		        estado = 3;
+		    } 
+		    else{
+		      estado = 2;
+		    }
 	    }
-	    if(y <=0){
-	      if(estado == 0){
-		    estado = 3;
-		  } 
-		  else{
-		    estado = 2;
-		  }
-	    }
-	    break;
-	    case 0:
-	      x--;
-	      y--;
-		  if(x<=(240-21)){
+	    break;	  
+
+        case 2:
+	      x++;
+	      y++;
+		  if(x>=(240-21)){
 		    estado = 0;
 		  }
-	  
-	    break;
-	    case 3:
-		x--;
-		y++;
-		case 2:
-		x++;
-		y--;
+		  if(y>=(240-21)){
+			if (estado==0){
+				estado = 1;
+			}
+			else{
+				estado = 3;
+			}
+		  }
+
+		  
       }
 }
 void task2 (void){
+  printbmp(x,y, DIDICOLHER);
+  paintscr(0,160,192);
+
+    switch(estado){
+		case 1:  
+		    x--;
+		    y--;
+
+			if(x<=0){
+				estado = 3;	
+			}
+			if(y<=0){
+              if(estado == 3){
+				estado = 2;
+			  }
+			}
+			else{
+				estado = 0;
+			}
+	
+
+	    case 0:  
+		    x--;
+		    y++;
+			if(x<=0){
+				estado = 1;
+			}
+			if(y>=(240-21)){
+				if(estado == 1)
+				estado = 3;
+			}
+			else{
+				estado = 2;
+			}
+		break;	    
+    
+        case 3:  
+	        x++;
+	        y--;
+	        if(x>=(240-21)){
+	          estado = 0;
+	        }
+	        if(y <=0){
+	          if(estado == 0){
+		        estado = 3;
+		    } 
+		    else{
+		      estado = 2;
+		    }
+	    }
+	    break;	  
+
+        case 2:
+	      x++;
+	      y++;
+		  if(x>=(240-21)){
+		    estado = 0;
+		  }
+		  if(y>=(240-21)){
+			if (estado==0){
+				estado = 1;
+			}
+			else{
+				estado = 3;
+			}
+		  }
+
+		  
+      }
 }
 
-void task3(void){ 
+void task3(void){
+  printbmp(x,y, DIDICOLHER);
+  paintscr(0,160,192);
+
+    switch(estado){
+		case 1:  
+		    x--;
+		    y--;
+
+			if(x<=0){
+				estado = 3;	
+			}
+			if(y<=0){
+              if(estado == 3){
+				estado = 2;
+			  }
+			}
+			else{
+				estado = 0;
+			}
+	
+
+	    case 0:  
+		    x--;
+		    y++;
+			if(x<=0){
+				estado = 1;
+			}
+			if(y>=(240-21)){
+				if(estado == 1)
+				estado = 3;
+			}
+			else{
+				estado = 2;
+			}
+		break;	    
+    
+        case 3:  
+	        x++;
+	        y--;
+	        if(x>=(240-21)){
+	          estado = 0;
+	        }
+	        if(y <=0){
+	          if(estado == 0){
+		        estado = 3;
+		    } 
+		    else{
+		      estado = 2;
+		    }
+	    }
+	    break;	  
+
+        case 2:
+	      x++;
+	      y++;
+		  if(x>=(240-21)){
+		    estado = 0;
+		  }
+		  if(y>=(240-21)){
+			if (estado==0){
+				estado = 1;
+			}
+			else{
+				estado = 3;
+			}
+		  }
+
+		  
+      }
 }
